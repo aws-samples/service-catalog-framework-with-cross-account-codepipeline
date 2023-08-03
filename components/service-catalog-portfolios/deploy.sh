4eba956360f752b6511b59afbdca4275c82a85a7
Karl Thomas <68962276+karldthomas@users.noreply.github.com>
2023-08-03 18:44:28 -0400

    Update deploy.sh

--- components/service-catalog-portfolios/deploy.sh
+++ components/service-catalog-portfolios/deploy.sh
@@ -1,23 +1,48 @@
 set -e
 
-export TagOptionsFile="$1"
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --tag-options-file)
+            TagOptionsFile="$2"
+            shift 2
+            ;;
+        --parameters-file)
+            ParametersFile="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            exit 1
+            ;;
+    esac
+done
+
+rm service-catalog-portfolio.yml.tmp || true
 
 
-
-python3 $FrameworkScriptsDir/replace-environment-vars.py \
+if [ -v TagOptionsFile ]; then
+  python3 $FrameworkScriptsDir/replace-environment-vars.py \
                     --json-file $TagOptionsFile > replaced-tag-options-vars.tmp
 
-python3 $FrameworkScriptsDir/create-tag-options.py \
-  --config-file replaced-tag-options-vars.tmp \
-  --template-file  ./service-catalog-portfolio.yml > service-catalog-portfolio.yml.tmp
+  python3 $FrameworkScriptsDir/create-tag-options.py \
+    --config-file replaced-tag-options-vars.tmp \
+    --template-file  ./service-catalog-portfolio.yml > service-catalog-portfolio.yml.tmp
+else
+  cp ./service-catalog-portfolio.yml service-catalog-portfolio.yml.tmp
+fi
 
+python3 $FrameworkScriptsDir/replace-environment-vars.py \
+                    --json-file $ParametersFile > replaced-vars.tmp
+
+export Parameters=$(python3 $FrameworkScriptsDir/convert-config-to-kv-pairs.py --json-file replaced-vars.tmp --key-value-type parameters)
 
 
 sam deploy  --template-file  service-catalog-portfolio.yml.tmp \
                         --stack-name $PortfolioStackName \
                         --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND \
                         --resolve-s3 \
-                        --parameter-overrides Nonce=$RANDOM \
+                        --parameter-overrides Nonce=$RANDOM "$Parameters" \
                         --no-fail-on-empty-changeset 
 
 
