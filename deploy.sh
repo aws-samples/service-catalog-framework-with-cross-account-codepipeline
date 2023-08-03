dd495b62b51ba937a94aa2e15fcf8431d5938396
Karl Thomas <68962276+karldthomas@users.noreply.github.com>
2023-08-03 18:42:55 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,20 @@ fi
 
 export TagFile=$BASEDIR/configuration/tag-options.json
 
+
 echo "Deploying Lambdas"
 cd components/lambdas
 bash deploy.sh 
 cd $BASEDIR
 
-export PortfolioStackName="service-catalog-portfolio-codepipeline"
+export PortfolioStackName="service-catalog-portfolio-admin"
 echo "Deploying Service Catalog Portfolio"
 cd components/service-catalog-portfolios 
-# We could use a separate tag file here to configure the Tag Options for the portfolio
-bash deploy.sh $TagFile
+bash deploy.sh --tag-options-file $TagFile --parameters-file $BASEDIR/portfolio-admin-config.json
+export PortfolioStackName="service-catalog-portfolio-enduser"
+bash deploy.sh --parameters-file $BASEDIR/portfolio-enduser-config.json
+
+
 cd $BASEDIR
 
 
@@ -24,3 +28,13 @@ export FrameworkScriptsDir="$BASEDIR/scripts"
 
 
 bash deploy-repository-products.sh
+
+# Deploy resources needed to automated sharing portfolios
+
+cd components/shared-products-resources
+bash deploy.sh --spoke-accounts $SpokeAccounts \
+               --devops-account $(aws sts get-caller-identity  | jq -r ".Account") \
+               --regions us-east-1
+
+cd ..
+
